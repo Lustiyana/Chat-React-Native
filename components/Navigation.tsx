ae527b1f7f8bd64a06b8c71f743303df1852eb81
Lustiyana <Lustiyana1801@gmail.com>
2023-08-15 20:45:27 +0700

    Customize header room chat

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,15 +6,86 @@ import StatusScreen from '../pages/StatusScreen';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {createStackNavigator} from '@react-navigation/stack';
 import RoomChat from '../pages/RoomChat';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, TextInput} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function ChatStack() {
+  const [openSearch, setOpenSearch] = useState(false);
+  const navigation = useNavigation();
+  const phoneButton = () => {
+    return <Icon name="call-outline" size={24} />;
+  };
+  const searchButton = () => {
+    return (
+      <TouchableOpacity onPress={() => setOpenSearch(true)}>
+        <Icon name="search-outline" size={24} />
+      </TouchableOpacity>
+    );
+  };
+
+  const headerRight = () => {
+    return (
+      <View style={{width: '100%', paddingRight: 16}}>
+        {!openSearch ? (
+          <View
+            style={{flexDirection: 'row', gap: 12, justifyContent: 'flex-end'}}>
+            {searchButton()}
+            {phoneButton()}
+          </View>
+        ) : (
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <TextInput
+              placeholder="Search..."
+              style={{
+                borderWidth: 1,
+                borderBottomColor: 'black',
+                borderTopWidth: 0,
+                borderRightWidth: 0,
+                borderLeftWidth: 0,
+                flex: 1,
+              }}
+            />
+            <View style={{flexDirection: 'row'}}>
+              <Icon name="chevron-up-outline" size={24} />
+              <Icon name="chevron-down-outline" size={24} />
+            </View>
+          </View>
+        )}
+      </View>
+    );
+  };
+
+  const headerLeft = () => {
+    return (
+      <TouchableOpacity
+        style={{paddingLeft: 16}}
+        onPress={() => {
+          if (openSearch) {
+            setOpenSearch(false);
+          } else {
+            navigation.goBack();
+          }
+        }}>
+        <Icon name="arrow-back-outline" size={24} />
+      </TouchableOpacity>
+    );
+  };
   return (
     <Stack.Navigator>
       <Stack.Screen name="Chats" component={TabNav} />
-      <Stack.Screen name="Room Chat" component={RoomChat} />
+      <Stack.Screen
+        name="Room Chat"
+        component={RoomChat}
+        options={{
+          title: openSearch ? '' : 'Gladys',
+          headerRight: headerRight,
+          headerLeft: headerLeft,
+        }}
+      />
     </Stack.Navigator>
   );
 }
